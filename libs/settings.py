c83583de66afcb0cadf216b6a1dd3d170e12ba36
illi4 <illi4.a@gmail.com>
2021-03-14 16:11:01 +1100

    Other conditions made softer as well

--- libs/settings.py
+++ libs/settings.py
@@ -1,5 +1,5 @@
 asx_instruments_url = "https://www.marketindex.com.au/asx-listed-companies"
 price_min = 0.01
-price_max = 1
+price_max = 5
 overextended_threshold_percent = 200
 tzinfo = "Australia/Sydney"
