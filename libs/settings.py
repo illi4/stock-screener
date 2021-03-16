d50a191147ac09e3971ceba96a0289f8d1c51d84
illi4 <illi4.a@gmail.com>
2021-03-16 20:35:23 +1100

    Price 1 to 3

--- libs/settings.py
+++ libs/settings.py
@@ -1,7 +1,7 @@
 asx_instruments_url = "https://www.marketindex.com.au/asx-listed-companies"
 asx_stock_url = "https://www.marketindex.com.au/asx"
 price_min = 0.01
-price_max = 5
+price_max = 3
 overextended_threshold_percent = 200
 minimum_volume_level = 500000
 tzinfo = "Australia/Sydney"
