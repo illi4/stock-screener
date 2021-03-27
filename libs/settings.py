15177468413693eb25f6899b6cb03637756d8a81
illi4 <illi4.a@gmail.com>
2021-03-27 21:51:29 +1100

    Price min to 0.005

--- libs/settings.py
+++ libs/settings.py
@@ -1,7 +1,7 @@
 asx_instruments_url = "http://eoddata.com/stocklist/ASX"
 nasdaq_instruments_url = "http://eoddata.com/stocklist/NASDAQ"
 asx_stock_url = "https://www.marketindex.com.au/asx"
-price_min = 0.01
+price_min = 0.005
 price_max = 100
 overextended_threshold_percent = 200
 minimum_volume_level = 500000
