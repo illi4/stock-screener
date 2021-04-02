7211a7259983f941d4b8435bf5e9c1cf351d6b8e
illi4 <illi4.a@gmail.com>
2021-04-02 15:41:40 +1100

    Monitor finished

---
+++ monitor.py
@@ -0,0 +1,60 @@
+import libs.gsheetobj as gsheetsobj
+from libs.stocktools import get_stock_data
+from libs.techanalysis import MA
+import arrow
+
+wb = "Trading journal 2021"
+
+
+def check_positions():
+    alerted_positions = []
+    for exchange in ["ASX", "NASDAQ"]:
+
+        if exchange == "NASDAQ":
+            stock_suffix = ""
+        elif exchange == "ASX":
+            stock_suffix = ".AX"
+
+        ws = gsheetsobj.sheet_to_df(wb, exchange)
+
+        for index, row in ws.iterrows():
+            if (
+                row["Outcome"] == ""
+            ):  # exclude the ones where we have results already, check if price falls below MA10
+                stock_code = row["Stock"]
+                entry_date = arrow.get(row["Entry date"], "DD/MM/YY").datetime.date()
+                ohlc_daily, volume_daily = get_stock_data(f"{stock_code}{stock_suffix}")
+                ma10 = MA(ohlc_daily, 10)
+
+                mergedDf = ohlc_daily.merge(ma10, left_index=True, right_index=True)
+                mergedDf.dropna(inplace=True, how="any")
+
+                mergedDf["close_below_ma"] = mergedDf["close"].lt(
+                    mergedDf["ma10"]
+                )  # LT is lower than
+
+                mergedDf["timestamp"] = mergedDf["timestamp"].dt.date
+                mergedDf = mergedDf[
+                    mergedDf["timestamp"] >= entry_date
+                ]  # only look from the entry date
+
+                alert = True in mergedDf["close_below_ma"].values
+                if alert:
+                    alerted_positions.append(f"{stock_code} ({exchange})")
+                    print(f"{stock_code} ({exchange}) [{entry_date}]: alert")
+                else:
+                    print(f"{stock_code} ({exchange}) [{entry_date}]: on track")
+
+    return alerted_positions
+
+
+if __name__ == "__main__":
+    print("Checking positions...")
+    alerted_positions = check_positions()
+    print()
+    if len(alerted_positions) == 0:
+        print("No alerts")
+    else:
+        print("Exit rules triggered for:")
+        for position in alerted_positions:
+            print(f"- {position}")
