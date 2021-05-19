134633b99e6a9da354d777f834d19fb9bee0ce71
illi4 <illi4.a@gmail.com>
2021-05-20 08:36:55 +1000

    Continuing with the simulator

--- simulator.py
+++ simulator.py
@@ -9,14 +9,15 @@ exchange = "ASX"
 confidence_filter = [8, 9]
 penny_filter = ["Y", "N"]
 capital = 5000
-commission = 5
+commission = 10
 
 # Variations to go through
 simultaneous_positions = [2, 3, 4, 5]
 variant_names = ["control", "test_a", "test_b", "test_c", "test_d"]
 # exit_threshold_variants = ... # todo later
+take_profit_levels = [0.25, 0.5, 0.9]
 start_date = "2021-04-01"
-end_date = "2021-05-01"
+end_date = "2021-05-20"
 
 # Sheet columns for the Gsheet
 sheet_columns = [
@@ -41,6 +42,7 @@ sheet_columns = [
     "test_d_price",
     "test_d_exit_date",
     "test_d_result_%",
+    "max_level_reached",
     "comments",
     "time_in_trade_control",
     "time_in_trade_test_a",
@@ -70,6 +72,7 @@ def convert_types(ws):
     ws[num_cols] = ws[num_cols].apply(pd.to_numeric, errors="coerce")
     ws = ws.loc[ws["confidence"].isin(confidence_filter)]
     ws = ws.loc[ws["penny_stock"].isin(penny_filter)]
+    ws["max_level_reached"] = ws["max_level_reached"].apply(p2f)
 
     ws["entry_date"] = pd.to_datetime(
         ws["entry_date"], format="%d/%m/%y", errors="coerce"
@@ -89,10 +92,8 @@ def convert_types(ws):
 
 
 if __name__ == "__main__":
-    print("This solution will remain incomplete until there is an actual need for it")
-    exit(0)
 
-    print("Reading the values...")
+    print("reading the values...")
 
     # This is working ok
     ws = gsheetsobj.sheet_to_df(gsheet_name, f"{exchange}")
@@ -115,33 +116,55 @@ if __name__ == "__main__":
     minimum_value = current_capital
     winning_trades, losing_trades = [], []
 
-    # Starting
-    print(f"Simulating {current_variant.capitalize()}")
+    # Starting for a variant
+    print(f"simulating {current_variant.lower()}")
     start_date_dt = datetime.strptime(start_date, "%Y-%m-%d")
     end_date_dt = datetime.strptime(end_date, "%Y-%m-%d")
     current_date_dt = start_date_dt
 
     # Note (todo) to replace test D with the values same as in control if empty
+    # Todo: grab the prices and handle the take_profit_levels
+
+    # Balance for the start of the period which will then be updated
+    balances = dict()
+    balances[start_date_dt.strftime("%d/%m/%Y")] = current_capital
+
+    # Iterating over days
     while current_date_dt < end_date_dt:
 
+        previous_date_month = current_date_dt.strftime("%m")
         current_date_dt = current_date_dt + timedelta(days=1)
-        print(current_date_dt, current_positions)
+        current_date_month = current_date_dt.strftime("%m")
 
-        day_entries = ws.loc[ws["entry_date"] == current_date_dt.date]
+        if previous_date_month != current_date_month:
+            balances[current_date_dt.strftime("%d/%m/%Y")] = current_capital
+
+        print(current_date_dt, "| positions: ", current_positions)
+        day_entries = ws.loc[ws["entry_date"] == current_date_dt]
         for key, elem in day_entries.iterrows():
-            positions_held += 1
-            current_positions.add(elem["stock"])
-            print("Entry", elem["stock"], "| positions held", positions_held)
 
-        day_exits = ws.loc[ws[f"{current_variant}_exit_date"] == current_date_dt.date]
+            if len(current_positions) + 1 > current_simultaneous_positions:
+                print(f"max possible positions held, skipping {elem['stock']}")
+            else:
+                positions_held += 1
+                current_positions.add(elem["stock"])
+                print("-> entry", elem["stock"], "| positions held", positions_held)
+                print(f"accounting for the trade price: ${commission}")
+                current_capital -= commission
+
+        day_exits = ws.loc[ws[f"{current_variant}_exit_date"] == current_date_dt]
         for key, elem in day_exits.iterrows():
-            current_positions.remove(elem["stock"])
-            positions_held -= 1
-            result = elem[f"{current_variant}_result_%"]
-            print(
-                "Exit", elem["stock"], "| positions held", positions_held, "| ", result
-            )
-            current_capital = current_capital * (
-                1 + elem[f"{current_variant}_result_%"] / current_simultaneous_positions
-            )
-            print("Capital:", current_capital)
+            if elem["stock"] in current_positions:
+                current_positions.remove(elem["stock"])
+                positions_held -= 1
+                result = elem[f"{current_variant}_result_%"]
+                print(f'-> exit {elem["stock"]} | result: {result:.2%} | positions held {positions_held}')
+                current_capital = current_capital * (
+                    1 + elem[f"{current_variant}_result_%"] / current_simultaneous_positions
+                )
+                print(f"accounting for the trade price: ${commission}")
+                current_capital -= commission
+                print(f"balance: ${current_capital}")
+
+    balances[current_date_dt.strftime("%d/%m/%Y")] = current_capital  # for the end date
+    print("result:", balances)
