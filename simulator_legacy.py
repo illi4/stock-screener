4a2f452bba95bc65ae1a3a55965e90b9c113dc0c
illi4 <illi4.a@gmail.com>
2022-04-25 18:37:15 +1000

    Using correct price in rollback

--- simulator_legacy.py
+++ simulator_legacy.py
@@ -498,6 +498,7 @@ def failsafe_trigger_rollback(sim, stock_prices, current_date_dt):
 
                     # We should use the correct price as something may just open very low
                     price_to_use = min(curr_row["open"].iloc[0], failsafe_rollback_level)
+                    print(f'-- using the price as a minimum of {curr_row["open"].iloc[0]} and {failsafe_rollback_level}')
                     failback_triggers.append([position, price_to_use])
 
     return failback_triggers
